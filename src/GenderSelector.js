271c7d03b893dd1513cbcc2171f1dae4332b0038
Murat Demirtas <muratdemirtas2001@gmail.com>
2021-05-05 19:26:09 +0100

    addding media query

--- src/GenderSelector.js
+++ src/GenderSelector.js
@@ -9,14 +9,16 @@ function GenderSelector({
 }) {
   return (
     <>
-      <div className={genderPictureClass[0] ? "babyIcon" : "babyIcon"}>
-        <FaBabyCarriage color="gray" size="2em" onClick={resetSearch} />
-      </div>
-      <div className={genderPictureClass[0] ? "babyIcon" : "babyIcon"}>
-        <FaBabyCarriage color="blue" size="2em" onClick={displayBoyNames} />
-      </div>
-      <div className="babyIcon">
-        <FaBabyCarriage color="pink" size="2em" onClick={displayGirlNames} />
+      <div className="icon-wrapper">
+        <div className={genderPictureClass[0] ? "babyIcon" : "babyIcon"}>
+          <FaBabyCarriage color="gray" size="2em" onClick={resetSearch} />
+        </div>
+        <div className={genderPictureClass[0] ? "babyIcon" : "babyIcon"}>
+          <FaBabyCarriage color="blue" size="2em" onClick={displayBoyNames} />
+        </div>
+        <div className="babyIcon">
+          <FaBabyCarriage color="pink" size="2em" onClick={displayGirlNames} />
+        </div>
       </div>
       {/* <img
         className={genderPictureClass[0] ? "gender-picture" : ""}
